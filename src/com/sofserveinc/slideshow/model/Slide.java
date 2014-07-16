6ca7f4ec3c3d43a86f208daed279952b52c6a33e
erik2013 <erik.yuntantyo@gmail.com>
2014-07-17 00:38:47 +0700

    Updated slide model.
    Updated slideshow model.

--- src/com/sofserveinc/slideshow/model/Slide.java
+++ src/com/sofserveinc/slideshow/model/Slide.java
@@ -1,5 +1,99 @@
 package com.sofserveinc.slideshow.model;
 
-public class Slide {
+/**
+ * Slide model.
+ * 
+ * @author Erik P. Yuntantyo
+ */
+public final class Slide {
+    /**
+     * Slide title.
+     */
+    private String title;
     
+    /**
+     * Slide subtitle.
+     */
+    private String subtitle;
+    
+    /**
+     * Slide content.
+     */
+    private Object content;
+    
+    /**
+     * Slide type.
+     */
+    private String type;
+    
+    /**
+     * Slide format.
+     */
+    private String format;
+    
+    /**
+     * Module constructor.
+     * 
+     * @param title    Slide title.
+     * @param subtitle Slide subtitle.
+     * @param content  Slide content.
+     * @param type     Slide type.
+     * @param format   Slide data format.
+     */
+    public Slide(final String title,
+                 final String subtitle,
+                 final Object content,
+                 final String type,
+                 final String format) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.content = content;
+        this.type = type;
+        this.format = format;
+    }
+    
+    /**
+     * Gets slide title.
+     * 
+     * @return the slide title.
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    /**
+     * Gets slide subtitle.
+     * 
+     * @return the slide subtitle.
+     */
+    public String getSubtitle() {
+        return subtitle;
+    }
+    
+    /**
+     * Gets slide content.
+     * 
+     * @return the slide content.
+     */
+    public Object getContent() {
+        return content;
+    }
+    
+    /**
+     * Gets slide type.
+     * 
+     * @return the slide type.
+     */
+    public String getType() {
+        return type;
+    }
+    
+    /**
+     * Gets slide format.
+     * 
+     * @return the slide format.
+     */
+    public String getFormat() {
+        return format;
+    }
 }
--- src/com/sofserveinc/slideshow/model/Slideshow.java
+++ src/com/sofserveinc/slideshow/model/Slideshow.java
@@ -1,5 +1,39 @@
 package com.sofserveinc.slideshow.model;
 
-public class Slideshow {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Slideshow model which contains list of slide model.
+ * 
+ * @author Erik P. Yuntantyo
+ */
+public final class Slideshow {
+    /**
+     * List of slide models.
+     */
+    private List<Slide> slides;
     
+    /**
+     * Module constructor.
+     * 
+     * @param slides Array of slide models.
+     */
+    public Slideshow(final Slide... slides) {
+        if ((slides != null) && (slides.length > 0)) {
+            this.slides = Arrays.asList(slides);
+        } else {
+            this.slides = new ArrayList<>();
+        }
+    }
+
+    /**
+     * Gets list of slide.
+     * 
+     * @return the list of slide.
+     */
+    public List<Slide> getSlides() {
+        return slides;
+    }    
 }
