a5e120ed757892c7ce99ab98ea1f5effa772ce55
erik <erik.yuntantyo@gmail.com>
2014-07-18 17:55:40 +0700

    Updated slideshow generator module.

--- src/com/sofserveinc/slideshow/SlideshowGenerator.java
+++ src/com/sofserveinc/slideshow/SlideshowGenerator.java
@@ -1,18 +1,8 @@
 package com.sofserveinc.slideshow;
 
-import java.io.File;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.sofserveinc.slideshow.contract.SlideGeneration;
-import com.sofserveinc.slideshow.dao.HtmlFileSaver;
-import com.sofserveinc.slideshow.dao.JsonFileLoader;
-import com.sofserveinc.slideshow.model.Slideshow;
-import com.sofserveinc.slideshow.service.HtmlFileSaverService;
-import com.sofserveinc.slideshow.service.JsonFileLoaderService;
-import com.sofserveinc.slideshow.service.SlideGenerationService;
 
 /**
  * Slideshow generator.
@@ -21,26 +11,42 @@ import com.sofserveinc.slideshow.service.SlideGenerationService;
  * @author Erik P. Yuntantyo
  */
 @Configuration
-public final class SlideshowGenerator {
+public class SlideshowGenerator {
+    /**
+     * Generator.
+     * 
+     * @return The slideshow generator module.
+     */
+    @Bean
+    public SlideshowGenerator generator() {
+        return this;
+    }
+    
     /**
      * Generate slideshow.
      * 
      * @param jsonPath   Json data file path.
      * @param htmlFolder Html file folder.
      */
-    @Bean
-    public void generate(final String jsonPath, final String htmlFolder) {
-        JsonFileLoader jsonFileLoader = new JsonFileLoaderService();
-        SlideGeneration slideGeneration = new SlideGenerationService();
-        HtmlFileSaver htmlFileSaver = new HtmlFileSaverService();
-        
-        String jsonFile = new File(jsonPath).getName();
-        Slideshow slideshow = jsonFileLoader.load(jsonPath);
-        String html = slideGeneration.generate(slideshow);
-        String destinationPath = new File(htmlFolder
-                                          + jsonFile.substring(0, jsonFile.lastIndexOf(".")))
-                                     .getAbsolutePath() + ".html";
-        
-        htmlFileSaver.save(html, destinationPath);
+    public final void generate(final String jsonPath, final String htmlFolder) {
+        new Thread(new Runnable() {            
+            @Override
+            public void run() {
+                /*JsonFileLoader jsonFileLoader = new JsonFileLoaderService();
+                SlideGeneration slideGeneration = new SlideGenerationService();
+                HtmlFileSaver htmlFileSaver = new HtmlFileSaverService();
+                
+                String jsonFile = new File(jsonPath).getName();
+                Slideshow slideshow = jsonFileLoader.load(jsonPath);
+                String html = slideGeneration.generate(slideshow);
+                String destinationPath = new File(htmlFolder
+                                                  + jsonFile.substring(0, jsonFile.lastIndexOf(".")))
+                                             .getAbsolutePath() + ".html";
+                
+                htmlFileSaver.save(html, destinationPath);*/
+                
+                Logger.getLogger(SlideshowGenerator.class).debug("Succeeded generate " + jsonPath);
+            }
+        }).start();
     }
 }
