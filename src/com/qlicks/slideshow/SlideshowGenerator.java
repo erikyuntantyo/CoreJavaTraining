b852ec29778fee9339c0c9427b4779cfe66980bf
erik2013 <erik.yuntantyo@gmail.com>
2014-07-19 01:06:30 +0700

    Updated slide generator.

--- src/com/qlicks/slideshow/SlideshowGenerator.java
+++ src/com/qlicks/slideshow/SlideshowGenerator.java
@@ -1,8 +1,19 @@
 package com.qlicks.slideshow;
 
+import java.io.File;
+
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import com.qlicks.slideshow.contract.SlideGeneration;
+import com.qlicks.slideshow.dao.HtmlFileSaver;
+import com.qlicks.slideshow.dao.JsonFileLoader;
+import com.qlicks.slideshow.model.Slideshow;
+import com.qlicks.slideshow.service.HtmlFileSaverService;
+import com.qlicks.slideshow.service.JsonFileLoaderService;
+import com.qlicks.slideshow.service.SlideGenerationService;
 
 /**
  * Slideshow generator.
@@ -13,13 +24,46 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SlideshowGenerator {
     /**
-     * Generator.
+     * Get slide generation.
      * 
-     * @return The slideshow generator module.
+     * @return the slide generation.
+     */
+    @Bean
+    @Scope("prototype")
+    public SlideGeneration generation() {
+        return new SlideGenerationService();
+    }
+    
+    /**
+     * Get slideshow generator.
+     * 
+     * @return the slideshow generator module.
      */
     @Bean
     public SlideshowGenerator generator() {
         return this;
+    }
+    
+    /**
+     * Get json file loader.
+     * 
+     * @return the json file loader.
+     */
+    @Bean
+    @Scope("prototype")
+    public JsonFileLoader loader() {
+        return new JsonFileLoaderService();
+    }
+    
+    /**
+     * Get html file saver.
+     * 
+     * @return the html file saver.
+     */
+    @Bean
+    @Scope("prototype")
+    public HtmlFileSaver saver() {
+        return new HtmlFileSaverService();
     }
     
     /**
@@ -29,23 +73,28 @@ public class SlideshowGenerator {
      * @param htmlFolder Html file folder.
      */
     public final void generate(final String jsonPath, final String htmlFolder) {
-        new Thread(new Runnable() {            
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                /*JsonFileLoader jsonFileLoader = new JsonFileLoaderService();
-                SlideGeneration slideGeneration = new SlideGenerationService();
-                HtmlFileSaver htmlFileSaver = new HtmlFileSaverService();
+                JsonFileLoader jsonFileLoader =
+                    FactoryManager.getBean("loader", JsonFileLoader.class);
+                SlideGeneration slideGeneration =
+                    FactoryManager.getBean("generation", SlideGeneration.class);
+                HtmlFileSaver htmlFileSaver =
+                    FactoryManager.getBean("saver", HtmlFileSaver.class);
                 
                 String jsonFile = new File(jsonPath).getName();
                 Slideshow slideshow = jsonFileLoader.load(jsonPath);
                 String html = slideGeneration.generate(slideshow);
-                String destinationPath = new File(htmlFolder
-                                                  + jsonFile.substring(0, jsonFile.lastIndexOf(".")))
-                                             .getAbsolutePath() + ".html";
+                String destinationPath =
+                    htmlFolder
+                        + jsonFile.substring(0, jsonFile.lastIndexOf("."))
+                        + ".html";
                 
-                htmlFileSaver.save(html, destinationPath);*/
+                htmlFileSaver.save(html, destinationPath);
                 
-                Logger.getLogger(SlideshowGenerator.class).debug("Succeeded generate " + jsonPath);
+                Logger.getLogger(SlideshowGenerator.class)
+                      .debug("Succeeded generate " + jsonPath + " to " + destinationPath);
             }
         }).start();
     }
