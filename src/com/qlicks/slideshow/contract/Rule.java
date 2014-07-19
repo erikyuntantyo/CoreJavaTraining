46ebae087b7dae16a590f74927b84679267a803b
erik2013 <erik.yuntantyo@gmail.com>
2014-07-20 01:56:22 +0700

    Refactored and updated rule.

---
+++ src/com/qlicks/slideshow/contract/Rule.java
@@ -0,0 +1,23 @@
+package com.qlicks.slideshow.contract;
+
+/**
+ * Interface of slide rule.
+ * 
+ * @author Erik P. Yuntantyo
+ */
+public interface Rule {
+    /**
+     * Gets content format.
+     * 
+     * @return the content format.
+     */
+    String getFormat();
+    
+    /**
+     * Generates content by rule.
+     * 
+     * @param content the content to generate.
+     * @return the generated content.
+     */
+    String generate(final String content);
+}
