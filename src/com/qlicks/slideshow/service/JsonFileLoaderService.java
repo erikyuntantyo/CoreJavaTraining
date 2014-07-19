0cc3a7c9f8b0791c50aaae96555a5b4c24173460
erik2013 <erik.yuntantyo@gmail.com>
2014-07-20 01:25:39 +0700

    Updated json file loader service and slide generation service.

--- src/com/qlicks/slideshow/service/JsonFileLoaderService.java
+++ src/com/qlicks/slideshow/service/JsonFileLoaderService.java
@@ -1,6 +1,17 @@
 package com.qlicks.slideshow.service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+import org.apache.log4j.Logger;
+
 import com.qlicks.slideshow.dao.JsonFileLoader;
+import com.qlicks.slideshow.model.Slide;
 import com.qlicks.slideshow.model.Slideshow;
 
 /**
@@ -11,7 +22,22 @@ import com.qlicks.slideshow.model.Slideshow;
 public final class JsonFileLoaderService implements JsonFileLoader {
     @Override
     public Slideshow load(final String jsonPath) {
-        // TODO Auto-generated method stub
+        try {
+            String json = new String(Files.readAllBytes(Paths.get(jsonPath)));
+            JSONObject jsonObject = JSONObject.fromObject(json);
+            Map<String, Class<?>> classMap = new HashMap<>();
+            
+            classMap.put("slides", Slide.class);
+            
+            return (Slideshow)JSONObject.toBean(jsonObject, Slideshow.class, classMap);
+        } catch (FileNotFoundException exc) {
+            Logger.getLogger(JsonFileLoader.class)
+                  .error("File " + jsonPath + " cannot be found.", exc);
+        } catch (IOException exc) {
+            Logger.getLogger(JsonFileLoader.class)
+                  .error("Error has been thrown while read json file.", exc);
+        }
+        
         return null;
     }
 }
--- src/com/qlicks/slideshow/service/SlideGenerationService.java
+++ src/com/qlicks/slideshow/service/SlideGenerationService.java
@@ -3,7 +3,10 @@
  */
 package com.qlicks.slideshow.service;
 
+import org.apache.log4j.Logger;
+
 import com.qlicks.slideshow.contract.SlideGeneration;
+import com.qlicks.slideshow.model.Slide;
 import com.qlicks.slideshow.model.Slideshow;
 
 /**
@@ -14,7 +17,11 @@ import com.qlicks.slideshow.model.Slideshow;
 public final class SlideGenerationService implements SlideGeneration {
     @Override
     public String generate(final Slideshow slideshow) {
-        // TODO Auto-generated method stub
+        for (Slide slide : slideshow.getSlides()) {
+            Logger.getLogger(SlideGeneration.class)
+                  .debug("Slide.title = " + slide.getTitle());
+        }
+        
         return null;
     }    
 }
