6b80948dc1cacf6ee039f9bd4828510ce1991e73
erik2013 <erik.yuntantyo@gmail.com>
2014-07-21 02:00:17 +0700

    Updated message format rule.

--- src/com/qlicks/slideshow/service/rule/MessageFormatRule.java
+++ src/com/qlicks/slideshow/service/rule/MessageFormatRule.java
@@ -1,5 +1,7 @@
 package com.qlicks.slideshow.service.rule;
 
+import java.text.MessageFormat;
+
 /**
  * The rule of message format type.
  * 
@@ -7,7 +9,7 @@ package com.qlicks.slideshow.service.rule;
  */
 public final class MessageFormatRule extends RuleBase {
     @Override
-    public String generate(final Object content) {
-        return content.toString();
+    public String generate(final Object content, final String format) {
+        return new MessageFormat(format).format(content);
     }
 }
