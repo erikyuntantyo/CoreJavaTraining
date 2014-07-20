adc1f406accef3cab58ac130fc359da62fb336a0
erik2013 <erik.yuntantyo@gmail.com>
2014-07-21 01:53:56 +0700

    Updated inspect rule.

--- src/com/qlicks/slideshow/service/rule/InspectRule.java
+++ src/com/qlicks/slideshow/service/rule/InspectRule.java
@@ -1,5 +1,8 @@
 package com.qlicks.slideshow.service.rule;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * The rule of inspect type.
  * 
@@ -8,6 +11,36 @@ package com.qlicks.slideshow.service.rule;
 public final class InspectRule extends RuleBase {
     @Override
     public String generate(final Object content, final String format) {
-        return content.toString();
+        StringBuilder builder = new StringBuilder();
+        
+        builder.append("<p>")
+               .append(content.getClass().getName())
+               .append("</p>\n")
+               .append("<div>Fields:</div>\n")
+               .append("<ul>\n");
+        
+        for (Field field : content.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            
+            builder.append("<li>")
+                   .append(field.getName())
+                   .append("</li>\n");
+        }
+        
+        builder.append("</ul>\n")
+               .append("<div>Methods:</div>\n")
+               .append("<ul>\n");
+        
+        for (Method method : content.getClass().getDeclaredMethods()) {
+            method.setAccessible(true);
+            
+            builder.append("<li>")
+                   .append(method.getName())
+                   .append("</li>\n");
+        }
+        
+        builder.append("</ul>\n");
+        
+        return builder.toString();
     }
 }
